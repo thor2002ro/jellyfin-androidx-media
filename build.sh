0df98011dad86484878380a2764bc553da2f4dbd
thor2002ro <1249056+thor2002ro@users.noreply.github.com>
2026-06-23 14:12:11 +0300

    Update Media3 and FFmpeg dependencies

--- build.sh
+++ build.sh
@@ -9,7 +9,13 @@ export ANDROID_NDK_PATH=$ANDROID_HOME/ndk/26.1.10909125
 export ANDROIDX_MEDIA_ROOT="${PWD}/media"
 export FFMPEG_MOD_PATH="${ANDROIDX_MEDIA_ROOT}/libraries/decoder_ffmpeg/src/main"
 export FFMPEG_PATH="${PWD}/ffmpeg"
-export ENABLED_DECODERS=(flac alac pcm_mulaw pcm_alaw mp3 aac ac3 eac3 dca mlp truehd)
+export ENABLED_DECODERS=(
+    flac alac pcm_mulaw pcm_alaw pcm_s16le pcm_f32le adpcm_ima_wav adpcm_ms
+    mp1 mp2 mp3 aac ac3 eac3 dca mlp truehd
+    vorbis opus amrnb amrwb wavpack ape speex gsm gsm_ms
+    mpeg1video mpeg2video flv h263 vp6 vp6f h264 hevc
+    msmpeg4v1 msmpeg4v2 msmpeg4v3 mpeg4 wmv1 wmv2 wmv3 vc1
+)
 
 # Create softlink to ffmpeg
 ln -sf "${FFMPEG_PATH}" "${FFMPEG_MOD_PATH}/jni/ffmpeg"
